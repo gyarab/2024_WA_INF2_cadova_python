e4acfde3b7f521371afeadd1a20cc248865e53fd
ZofieC <zofie.cadova@student.gyarab.cz>
2024-12-12 09:59:01 +0100

    upraveni vyjimky pro 1

--- test.py
+++ test.py
@@ -16,6 +16,9 @@ def is_prime(number):
         
     if number == 1:
         return False
+    
+    if number == 1.0:
+        return False
         
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
