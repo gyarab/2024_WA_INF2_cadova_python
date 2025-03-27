da474c02189e0869c3da8f5f8142bb0ad03489b5
ZofieC <zofie.cadova@student.gyarab.cz>
2025-03-27 10:07:09 +0100

    zmeny

--- content/admin.py
+++ content/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Article
+from .models import Band, Genre, Singer
 
-admin.site.register(Article)
+admin.site.register(Band)
+admin.site.register(Genre)
+admin.site.register(Singer)
--- content/views.py
+++ content/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Article
+from .models import Band, Genre, Singer
 
 import requests
 import json
 
 def homepage(request):
-    articles = Article.objects.all()
+    articles = Band.objects.all()
 
     return render(request, 'content/homepage.html', {'articles': articles})
 
-def article(request, id):
+def band(request, id):
     with open('articles.json', encoding='utf-8') as f:
-        articles = json.load(f)
+        bands = json.load(f)
 
-    article = articles[id]
-    return render(request, 'content/article.html', {'article': article})
+    band = bands[id]
+    return render(request, 'content/article.html', {'article': band})
+
+def genre(request, id):
+    with open('articles.json', encoding='utf-8') as f:
+        genres = json.load(f)
+
+    genre = genres[id]
+    return render(request, 'content/article.html', {'article': genre})
+
+def singer(request, id):    
+    with open('articles.json', encoding='utf-8') as f:
+        singers = json.load(f)
+
+    singer = singers[id]
+    return render(request, 'content/singer.html', {'singer': singer})
