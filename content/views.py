6d3361988f09ac7a76bc760cc01c0e27a945013f
ZofieC <zofie.cadova@student.gyarab.cz>
2025-05-15 10:33:32 +0200

    oprava

--- content/views.py
+++ content/views.py
@@ -12,21 +12,21 @@ def homepage(request):
     return render(request, 'content/homepage.html', {'articles': articles})
 
 def band(request, id):
-    with open('articles.json', encoding='utf-8') as f:
+    with open('content/fixtures/bands.json', encoding='utf-8') as f:
         bands = json.load(f)
 
     band = bands[id]
     return render(request, 'content/article.html', {'article': band})
 
 def genre(request, id):
-    with open('articles.json', encoding='utf-8') as f:
+    with open('content/fixtures/bands.json', encoding='utf-8') as f:
         genres = json.load(f)
 
     genre = genres[id]
     return render(request, 'content/genre.html', {'genre': genre})
 
 def singer(request, id):    
-    with open('articles.json', encoding='utf-8') as f:
+    with open('content/fixtures/bands.json', encoding='utf-8') as f:
         singers = json.load(f)
 
     singer = singers[id]
